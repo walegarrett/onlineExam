5b588d265efc7abf0fec4b47e1f9625e55a4b2e7
guoguanhui <2064737314@qq.com>
2022-04-25 21:29:48 +0800

    fix bugs

--- src/main/java/com/wale/exam/controller/FileUploadController.java
+++ src/main/java/com/wale/exam/controller/FileUploadController.java
@@ -150,7 +150,7 @@ public class FileUploadController {
         HttpSession session1=request.getSession();
         int uid= (int) session1.getAttribute("userid");
         String path=request.getSession().getServletContext().getRealPath("/uploads/");
-        path="E:\\wps\\专业实训\\exam\\onlineExam\\upload\\images";
+        path="E:\\IDEA\\IdeaProjects\\onlineExam\\upload\\images";
         //判断，该路径是否存在
         File file=new File(path);
         if(!file.exists()){
